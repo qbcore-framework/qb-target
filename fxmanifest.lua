c24d3cd2cd93aef5fbd39e15c7e6855c6260aeb1
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-07-24 20:06:14 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+dependency "PolyZone"
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',
+	'config.lua',
+	'client/*.lua',
+}
+
+server_script 'server/main.lua'
+
+files {
+	'html/*.html',
+	'html/css/*.css',
+	'html/js/*.js'
+}
