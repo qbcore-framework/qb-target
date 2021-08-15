2efb59ea8e606e657e369afff8a8e4c200f8fa12
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-16 00:13:01 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-version '2.4.2'
-
-dependency "PolyZone"
+version '2.4.3'
 
 ui_page 'html/index.html'
 
 client_scripts {
+	'@qb-core/import.lua',
 	'@PolyZone/client.lua',
 	'@PolyZone/BoxZone.lua',
 	'@PolyZone/EntityZone.lua',
@@ -21,4 +20,9 @@ files {
 	'html/*.html',
 	'html/css/*.css',
 	'html/js/*.js'
+}
+
+dependencies {
+	"PolyZone",
+	"qb-core"
 }
