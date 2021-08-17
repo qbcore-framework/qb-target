9bb579c96ad22a748224608e9c4525a837f60168
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-17 17:23:42 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'BerkieB'
 description 'bt-target rewritten with the help of qtarget, Archangelza1 and myself to make the best interaction system for FiveM whilst keeping the best optimization possible!'
-version '2.4.5'
+version '2.4.6'
 
 ui_page 'html/index.html'
 
