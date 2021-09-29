e456d55aefd5a9bfe8ce1c0e12af069467301dfa
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-09-29 21:32:36 +0200

    chore(manifest): Bump version to v2.6.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'BerkieB'
 description 'bt-target rewritten with the help of contributors to make the best interaction system for FiveM whilst keeping the best optimization possible!'
-version '2.6.1'
+version '2.6.2'
 
 ui_page 'html/index.html'
 
