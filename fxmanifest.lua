60dc60a3cfd37782e2ec8986eb42052e01ac3f2f
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-03-30 20:48:42 -0700

    Second round of conversion

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
-game 'gta5'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'BerkieB'
 description 'An optimized interaction system for FiveM, based on qtarget'
